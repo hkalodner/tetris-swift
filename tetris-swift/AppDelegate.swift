d1d1a8cddd4f45e68db09b80e60507aa1a46e60e
Harry Kalodner <harry.kalodner@gmail.com>
2014-10-16 23:04:33 -0400

    Initial Commit

---
+++ tetris-swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tetris-swift
+//
+//  Created by Harry Kalodner on 10/16/14.
+//  Copyright (c) 2014 Harry Kalodner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
